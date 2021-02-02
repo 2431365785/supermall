332583a7978d01f16f084e4e35611f1b4b2b3715
SunWeiLun <8099121+sunweilun@user.noreply.gitee.com>
2021-02-02 17:33:17 +0800

    axios封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,52 @@
 <template>
   <div>
-    <h1>首页</h1>
+    <!-- 顶部 -->
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+
+    <!-- 轮播图 -->
+    <swiper :list="banners" />
+
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+
+import { getHomeMultidata } from 'network/home'
+
 export default {
   name: 'Home',
   data() {
-    return {}
+    return {
+      banners: [],
+      recommends: [],
+    }
+  },
+  created() {
+    this.getHomeMultidata()
+  },
+  methods: {
+    /* 网络请求相关 */
+    // 获取轮播图等数据
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        console.log(res)
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    },
+  },
+  components: {
+    NavBar,
   },
 }
 </script>
 
 <style  scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
